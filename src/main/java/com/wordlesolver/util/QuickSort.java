f7776818d2334420d3d1057c5bbb48c56adb156f
aayu22809 <67443596+aayu22809@users.noreply.github.com>
2024-11-18 11:47:55 -0800

    Make efficiency changes Remove redundant log Change thread utilization

--- src/main/java/com/wordlesolver/util/QuickSort.java
+++ src/main/java/com/wordlesolver/util/QuickSort.java
@@ -5,22 +5,20 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-// Full Disclosure: This code is from https://www.geeksforgeeks.org/quick-sort/
 public class QuickSort {
     // Method to sort a Map by its values in descending order
     public static Map<String, Double> sortByValueDescending(Map<String, Double> map) {
-        // Convert the map into a list of entries
-        List<Map.Entry<String, Double>> entryList = new ArrayList<>(map.entrySet());
+        // Create a list from elements of HashMap
+        List<Map.Entry<String, Double>> list = new ArrayList<>(map.entrySet());
 
-        // Sort the list based on the values in descending order
-        entryList.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
+        // Sort the list
+        list.sort((o1, o2) -> (o2.getValue()).compareTo(o1.getValue()));
 
-        // Maintain the order in a LinkedHashMap
-        Map<String, Double> sortedMap = new LinkedHashMap<>();
-        for (Map.Entry<String, Double> entry : entryList) {
-            sortedMap.put(entry.getKey(), entry.getValue());
+        // put data from sorted list to hashmap
+        Map<String, Double> temp = new LinkedHashMap<>();
+        for (Map.Entry<String, Double> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
         }
-
-        return sortedMap;
+        return temp;
     }
 }
